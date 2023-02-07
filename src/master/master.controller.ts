589f13471a728e234192749db8af2b1ec00a7416
Vlad Avseev <vladavseev47@gmail.com>
2023-02-07 15:14:29 +0300

    add auth guard to invite master controller

--- src/master/master.controller.ts
+++ src/master/master.controller.ts
@@ -1,11 +1,13 @@
-import {Body, Controller, Post} from '@nestjs/common';
+import {Body, Controller, Post, UseGuards} from '@nestjs/common';
 import {MasterService} from "./master.service";
+import {JwtAuthGuard} from "../auth/guard/jwt-auth.guard";
 
 @Controller('master')
 export class MasterController {
     constructor(private masterService: MasterService) {}
 
     @Post()
+    @UseGuards(JwtAuthGuard)
     inviteMaster(@Body() email: string): Promise<{message: string}> {
         return this.masterService.invite(email)
     }
