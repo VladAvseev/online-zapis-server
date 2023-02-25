cffeb58ba0b58129b42eb46e48e6d3e9be1dcfa1
Vlad Avseev <vladavseev47@gmail.com>
2023-02-25 19:00:39 +0300

    add field image to user.teams

--- src/team/dto/response-user-team.dto.ts
+++ src/team/dto/response-user-team.dto.ts
@@ -4,10 +4,12 @@ export class ResponseUserTeamDto {
     readonly id: number;
     readonly title: string;
     readonly description: string | null;
+    readonly image: string;
 
     constructor(team: TeamModel) {
         this.id = team.id;
         this.title = team.title;
         this.description = team.description || null;
+        this.image = team.image;
     }
 }
